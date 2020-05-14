6babc9ae3923d234be2d4ca0d73eaeb332ab1cb6
SoyDiego <diegofranchina@gmail.com>
2020-05-14 04:30:11 +0200

    Layout and AddUser done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
+import shortid from "shortid";
+import Usertable from "./Components/Usertable";
+import AddUserForm from "./Components/AddUserForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const usersData = [
+		{ id: shortid.generate(), name: "Tania", username: "floppydiskette" },
+		{ id: shortid.generate(), name: "Craig", username: "siliconeidolon" },
+		{ id: shortid.generate(), name: "Ben", username: "benisphere" },
+	];
+
+	//State
+	const [users, setUsers] = useState(usersData);
+
+	//Agregar usuario
+	const addUser = (user) => {
+		user.id = shortid.generate();
+		setUsers([...users, user]);
+	};
+
+	return (
+		<Fragment>
+			<div className="container">
+				<h1>CRUD App with Hooks</h1>
+				<div className="flex-row">
+					<div className="flex-large">
+						<h2>Add user</h2>
+						<AddUserForm addUser={addUser} />
+					</div>
+					<div className="flex-large">
+						<h2>View users</h2>
+						<Usertable users={users} />
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
